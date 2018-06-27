35dfc1f246c55d0028f72bca1ae24360cb1b37f1
Nate Cottle <cottle.nate@gmail.com>
2018-06-27 15:10:29 -0700

    pass test for full word replace.

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -1,4 +1,20 @@
-def find_replace(sentence)
-  split_sentence = sentence.split(" ")
 
+class FindReplace
+  def initialize (phrase)
+    @phrase = phrase
+  end
+
+  def change (find, replace)
+    replace = {find => replace}
+    final_sentence_array = []
+    split_sentence = @phrase.split(" ")
+    split_sentence.each do |word|
+      if replace.to_s.include?(word) == true
+        final_sentence_array.push(replace.fetch(word))
+      else
+        final_sentence_array.push(word)
+      end
+    end
+    final_sentence_array.join(" ")
+  end
 end
--- spec/find_replace_spec.rb
+++ spec/find_replace_spec.rb
@@ -3,7 +3,12 @@ require 'find_replace'
 
 describe('find_replace') do
  it('take string and split into array') do
-   expect(find_replace("hello everyone")).to(eq(["hello", "everyone"]))
+   phrase = FindReplace.new("hello world")
+   expect(phrase.change("world", "universe")).to(eq("hello universe"))
  end
+
+ # it('find word based on user input') do
+ #   expect(find("everyone")).to(eq(true))
+ # end
 
 end
